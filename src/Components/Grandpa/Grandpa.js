d9cdd25c5ea40670764c1985755b5a5e009ee6e9
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-07-27 00:18:59 +0600

    props Delaying

--- src/Components/Grandpa/Grandpa.js
+++ src/Components/Grandpa/Grandpa.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Grandpa.css';
 import Father from '../Father/Father.js';
 import Uncle from '../Uncle/Uncle.js';
 import Aunty from '../Aunty/Aunty.js';
 
 const Grandpa = () => {
-    const house = 7;
+    const [house, setHouse] = useState(1);
+    const ornament = 'DiamondRing';
+
+    const handleBuyAHouse = () =>{
+        const newHouse = house+1;
+        setHouse(newHouse);
+    }
     return (
         <section className='grandpa' >
-            <h3>Grand pa</h3>
+            <h3>Grand pa</h3> 
+            <p>House: {house} <button onClick={handleBuyAHouse}> Buy a House</button></p>
             <div style={{ display: 'flex' }}>
 
-                <Father house={house}></Father>
+                <Father house={house} ornament={ornament}></Father>
                 <Uncle house={house}></Uncle>
                 <Aunty house={house}></Aunty>
             </div>
--- src/Components/MySelf/MySelf.js
+++ src/Components/MySelf/MySelf.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Special from '../Special/Special';
 
-const MySelf = ({house}) => {
+const MySelf = ({house,ornament}) => {
     return (
         <div>
             <h4>Me</h4>
             <p>House: {house}</p>
+            <Special ornament={ornament}></Special>
         </div>
     );
 };
